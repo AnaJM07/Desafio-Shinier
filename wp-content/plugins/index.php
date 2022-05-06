2bc6e5d7756dd09cfd6b815972983b8eb04e263f
AnaJM07 <anamartinis71@gmail.com>
2022-05-06 14:27:49 -0300

    Finalizando Plugin

--- wp-content/plugins/index.php
+++ wp-content/plugins/index.php
@@ -1,2 +1,24 @@
 <?php
-// Silence is golden.
+
+
+$url = " https://api.gold.shinier.com.br/api/login HTTP/1.1";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Content-Type: application/json",
+   "Content-Length: 0",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
+
+?>
